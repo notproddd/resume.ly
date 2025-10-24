53029de0b81d248358b4282ea6d66e53cdab3d2f
Manish Balayan <manish@Manishs-MacBook-Pro.local>
2025-10-24 16:34:57 +0530

    feat(ui): real-time validation, progress indicator, dark mode, and responsiveness improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  darkMode: 'class',
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./templates/**/*.{js,jsx,html}", "./examples/**/*.{js,json}"],
   theme: {
     extend: {},
